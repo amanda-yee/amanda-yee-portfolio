0b98a315fd117a86e3c32586402a9d65855de781
Amanda Yee <amandakyee@gmail.com>
2025-03-10 23:40:46 -0400

    import tailwind, move header into components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="app-header">
-        Amanda Yee
-      </header>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header className="">
+        Amanda Yee
+      </header>
+    );
+};
+
+export default Header;
