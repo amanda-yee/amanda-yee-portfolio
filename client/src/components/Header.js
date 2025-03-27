46deeb8dd5d587a6826bbc2ac2809e694c28213d
Amanda Yee <amandakyee@gmail.com>
2025-03-27 01:20:45 -0400

    remove home from header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,10 +6,9 @@ const Header = () => {
         </div> */}
         <div className="header">
           <ul className="list fade-in-diagonal">
-              {/* <li><a href="#home">home</a></li> */}
               <li><a href="#projects">projects</a></li>
               <li><a href="#about">about</a></li>
-              <li><a href="#film">film</a></li>
+              {/* <li><a href="#film">film</a></li> */}
               <li><a href="#contact">contact</a></li>
           </ul>
           <a href="#home" className="font-bold fade-in-diagonal">amanda yee</a>
