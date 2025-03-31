dc720a5dc1f8965e385e4a2be7aa5294fadeefa7
Amanda Yee <amandakyee@gmail.com>
2025-03-30 22:25:44 -0400

    have header appear when user is scrolling to top

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
 const Header = () => {
+  const [prevScrollY, setPrevScrollY] = useState(0); // State to store the previous scrollY value
+  const [showHeader, setShowHeader] = useState(true); // State to track whether the header should be visible
+    
+    useEffect(() => {
+      // Function to log the scroll position
+      
+      const handleScroll = () => {
+        const currScrollY = window.scrollY;
+
+        if (currScrollY < prevScrollY ) {
+          console.log("scrolling backwards");
+          setShowHeader(true);
+        } else {
+          setShowHeader(false);
+        }
+
+        // Update previous scrollY val
+        setPrevScrollY(currScrollY);
+      };
+
+      // Add the event listener when the component mounts
+      window.addEventListener('scroll', handleScroll);
+
+      // Clean up the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [prevScrollY]); // dependency array ensures this runs once when 'prevScrollY' changes
+
+
     return (
-      <header className="fixed top-0 left-0 w-full flex items-center justify-between py-6 w-full">
+      <header 
+        className={`fixed top-0 left-0 w-full flex items-center justify-between py-6 w-full z-50 transition-all duration-300 ${
+          showHeader ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
         <div className="ml-[5%]">
           <ul className="list fade-in-diagonal">
               <li><a href="#about">about</a></li>
