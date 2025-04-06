edbfaa8da27df6a5562468f34c52c65456a521c4
Amanda Yee <amandakyee@gmail.com>
2025-04-06 01:46:38 -0400

    comment out import

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import UnderConstruction from './UnderConstruction';
+// import UnderConstruction from './UnderConstruction';
 import React, { useState, useEffect } from 'react';
 
 const Header = () => {
