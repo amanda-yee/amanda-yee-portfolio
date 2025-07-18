cdee8c20ff8b9437b4f9152438353e5c5f605f8b
Amanda Yee <amandakyee@gmail.com>
2025-07-18 00:46:47 -0400

    change font sizing

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -4,11 +4,11 @@ const Intro = () => {
     return (
       <div className="flex flex-col text-center fade-in-diagonal pt-20">
 
-        <div className="text-6xl font-bold pb-8 font-custom">
+        <div className="text-4xl sm:text-6xl font-bold pb-8 font-custom">
           hi, i'm amanda
         </div>
 
-        <div className="text-4xl pb-8 font-custom">
+        <div className="text-3xl sm:text-4xl pb-8 font-custom">
           a web developer <br></br>
           & data scientist <br></br> <br></br>
           based in new york.
